da2c1fb96c426d0b6970a49387d084d375394f73
Felipe <120607893+f95f@users.noreply.github.com>
2023-01-19 22:21:22 -0300

    added complete subtraction mode game

--- game_1/results.js
+++ game_1/results.js
@@ -1,17 +1,19 @@
-
+let r_modo = document.getElementById("r_modo");
 let r_total = document.getElementById("r_total");
 let r_acertos = document.getElementById("r_acertos");
 let r_erros = document.getElementById("r_erros");
 let r_score = document.getElementById("r_score");
 let r_diffMax = document.getElementById("r_diff");
-let diff = sessionStorage.getItem("f_score")
+let diff = sessionStorage.getItem("f_score");
 let dificuldade = ''; 
 
 let setValores = function(){
 
+    r_modo.innerText = "Modo: " + sessionStorage.getItem("f_modo");
     r_score.innerText = diff;
     r_acertos.innerText = sessionStorage.getItem("f_acertos");
     r_total.innerText = sessionStorage.getItem("f_total");
+
    
     if(diff < 25){ dificuldade = '*'}
     else if(diff < 50){ dificuldade = '**'}
