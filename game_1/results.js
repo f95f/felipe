cde7e71986327bb073ac772af89b8dfb6b71e492
Felipe <120607893+f95f@users.noreply.github.com>
2023-01-19 21:20:05 -0300

    added first game files

---
+++ game_1/results.js
@@ -0,0 +1,26 @@
+
+let r_total = document.getElementById("r_total");
+let r_acertos = document.getElementById("r_acertos");
+let r_erros = document.getElementById("r_erros");
+let r_score = document.getElementById("r_score");
+let r_diffMax = document.getElementById("r_diff");
+let diff = sessionStorage.getItem("f_score")
+let dificuldade = ''; 
+
+let setValores = function(){
+
+    r_score.innerText = diff;
+    r_acertos.innerText = sessionStorage.getItem("f_acertos");
+    r_total.innerText = sessionStorage.getItem("f_total");
+   
+    if(diff < 25){ dificuldade = '*'}
+    else if(diff < 50){ dificuldade = '**'}
+    else if(diff < 100){ dificuldade = '***'}
+    else if(diff < 200){ dificuldade = '****'}    
+    else if(diff < 500){ dificuldade = '*****'}
+    else if(diff < 1000){ dificuldade = '!'}
+    else if(diff < 10000){ dificuldade = '!!'}
+    else{ dificuldade = '☠️'}
+
+    r_diffMax.innerText = dificuldade;
+}
